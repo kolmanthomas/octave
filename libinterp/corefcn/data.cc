ee4fb0a867fa94162911fb2ea32a818c70e2c135
kolmanthomas <realkolmanthomas@gmail.com>
2025-11-07 18:49:44 -0500

    Implement array concatenation of classdef objects (bug #44665).
    * data.cc: add includes for ov-classdef.h and cdef-utils.h.
    (attempt_type_conversion): modify function to look for classdef methods,
    and execute them if they are present.
    (do_single_type_concat_cdef): new function to concatenate a list of
    octave_classdef values of the same class.
    (do_class_concat): find out if dominant type is a classdef and, in that case,
    call do_single_type_concat_cdef.
    * test/classdef/class_pair.m: new class for testing.
    * test/classdef/class_pair_elem.m: new class for testing.
    * test/classdef/classdef.tst: add tests.
    * test/classdef/module.mk: add new files to the build system.

--- libinterp/corefcn/data.cc
+++ libinterp/corefcn/data.cc
@@ -43,6 +43,7 @@
 #include "oct-time.h"
 #include "quit.h"
 
+#include "cdef-utils.h"
 #include "Cell.h"
 #include "data.h"
 #include "defun.h"
@@ -52,6 +53,7 @@
 #include "interpreter.h"
 #include "oct-map.h"
 #include "ov-class.h"
+#include "ov-classdef.h"
 #include "ov-complex.h"
 #include "ov-cx-mat.h"
 #include "ov-cx-sparse.h"
@@ -2748,6 +2750,59 @@ do_single_type_concat_map (const octave_value_list& args,
 }
 
 static octave_value
+do_single_type_concat_cdef (const octave_value_list& args,
+                           int dim)
+{
+  // Concatenate a list of octave_classdef values of the same
+  // class along dimension dim
+
+  Array<cdef_object> arr;
+  int n_args = args.length ();
+
+  if (all_scalar_1x1 (args)) // optimize all scalars case.
+    {
+      // Optimize all scalars case.
+      dim_vector dv (1, 1);
+      if (dim == -1 || dim == -2)
+        dim = -dim - 1;
+      else if (dim >= 2)
+        dv.resize (dim+1, 1);
+      dv(dim) = n_args;
+
+      arr.clear (dv);
+
+      for (int j = 0; j < n_args; j++)
+        {
+          octave_quit ();
+
+          arr(j) = args(j).classdef_object_value ()->get_object ();
+        }
+    }
+  else
+    {
+      OCTAVE_LOCAL_BUFFER (Array<cdef_object>, array_list, n_args);
+
+      for (int j = 0; j < n_args; j++)
+        {
+          octave_quit ();
+
+          cdef_object obj = args(j).classdef_object_value ()->get_object ();
+          if (obj.is_array ())
+            array_list[j] = obj.array_value ();
+          else
+            array_list[j] = Array<cdef_object> (dim_vector (1,1), obj);
+        }
+
+      arr = Array<cdef_object>::cat (dim, n_args, array_list);
+    }
+
+  cdef_object obj_result = cdef_object (new cdef_object_array (arr));
+  obj_result.set_class (arr(0).get_class ());
+
+  return to_ov (obj_result);
+}
+
+static octave_value
 attempt_type_conversion (const octave_value& ov, std::string dtype)
 {
   octave_value retval;
@@ -2762,7 +2817,11 @@ attempt_type_conversion (const octave_value& ov, std::string dtype)
 
   symbol_table& symtab = interp.get_symbol_table ();
 
-  octave_value fcn = symtab.find_method (dtype, cname);
+  cdef_manager& cdef_mgr = interp.get_cdef_manager ();
+
+  auto fcn = cdef_mgr.find_method_symbol (dtype, cname);
+  if (!fcn.is_defined ())
+    fcn = symtab.find_method (dtype, cname);
 
   if (fcn.is_defined ())
     {
@@ -2788,29 +2847,60 @@ attempt_type_conversion (const octave_value& ov, std::string dtype)
     {
       // No conversion function available.  Try the constructor for the
       // dispatch type.
+      // We can't go purely through the symbol table here, we have to
+      // check the classdef manager as well for classdef methods
+      auto ctor = cdef_mgr.find_method_symbol (dtype, dtype);
 
-      fcn = symtab.find_method (dtype, dtype);
-
-      if (! fcn.is_defined ())
-        error ("no constructor for %s!", dtype.c_str ());
+      if (!ctor.is_defined ())
+        ctor = symtab.find_method (dtype, dtype);
 
       octave_value_list result;
 
-      try
+      // The following code looks fairly ugly because we have to handle old-style
+      // classes and classdef constructors separately
+      if (ctor.is_defined () && ctor.is_function ())
         {
-          result = interp.feval (fcn, ovl (ov), 1);
-        }
-      catch (execution_exception& ee)
-        {
-          error (ee, "%s constructor failed for %s argument", dtype.c_str (),
-                 cname.c_str ());
+        octave_function *ctor_fcn = ctor.function_value ();
+
+          if (ctor_fcn->is_legacy_constructor ())
+            {
+              // Old-style class constructor
+              octave::unwind_protect frame;
+
+              octave::interpreter_try (frame);
+
+              bool execution_error = false;
+
+              try
+                {
+                 result = interp.feval (ctor, ovl (ov), 1);
+                }
+              catch (const octave::execution_exception&)
+                {
+                  interp.recover_from_exception ();
+
+                  execution_error = true;
+                }
+            }
+          else
+            // Classdef constructor
+            {
+              auto cls = lookup_class (dtype, false, false);
+
+              try
+                {
+                  result = to_ov (cls.construct_object (ovl (ov)));
+                }
+              catch (execution_exception& ee)
+                {
+                  error (ee, "%s constructor failed for %s argument", dtype.c_str (),
+                         cname.c_str ());
+                }
+
+            }
         }
 
-      if (result.empty ())
-        error ("%s constructor failed for %s argument", dtype.c_str (),
-               cname.c_str ());
-
-      retval = result(0);
+       retval = result(0);
     }
 
   return retval;
@@ -2828,11 +2918,13 @@ do_class_concat (const octave_value_list& ovl,
 
   interpreter& interp = __get_interpreter__ ();
 
+  cdef_manager& cdef_mgr = __get_cdef_manager__ ();
+
   symbol_table& symtab = interp.get_symbol_table ();
 
-  octave_value fcn = symtab.find_method (cattype, dtype);
+  octave_value ov_fcn = symtab.find_method (cattype, dtype);
 
-  if (fcn.is_defined ())
+  if (ov_fcn.is_defined ())
     {
       // Have method for dominant type.  Call it and let it handle conversions.
 
@@ -2840,7 +2932,7 @@ do_class_concat (const octave_value_list& ovl,
 
       try
         {
-          tmp2 = interp.feval (fcn, ovl, 1);
+          tmp2 = interp.feval (ov_fcn, ovl, 1);
         }
       catch (execution_exception& ee)
         {
@@ -2876,12 +2968,22 @@ do_class_concat (const octave_value_list& ovl,
 
       tmp.resize (j);
 
-      octave_map m = do_single_type_concat_map (tmp, dim);
+      // See if dominant type is a classdef
+      cdef_class cdef = cdef_mgr.find_class (dtype, false);
 
-      std::string cname = tmp(0).class_name ();
-      std::list<std::string> parents = tmp(0).parent_class_name_list ();
+      if (cdef.ok ())
+          // Default classdef concat
+          retval = do_single_type_concat_cdef (tmp, dim);
+      else
+        {
+          // Default struct-based class concat
+          octave_map m = do_single_type_concat_map (tmp, dim);
 
-      retval = octave_value (new octave_class (m, cname, parents));
+          std::string cname = tmp(0).class_name ();
+          std::list<std::string> parents = tmp(0).parent_class_name_list ();
+
+          retval = octave_value (new octave_class (m, cname, parents));
+        }
     }
 
   return retval;
--- test/classdef/module.mk
+++ test/classdef/module.mk
@@ -15,6 +15,8 @@ classdef_TEST_FILES = \
   %reldir%/class_bug55766.m \
   %reldir%/class_bug66930A.m \
   %reldir%/class_bug66930B.m \
+  %reldir%/class_pair.m \
+  %reldir%/class_pair_elem.m \
   %reldir%/classdef.tst \
   %reldir%/foo_method_changes_property_size.m \
   %reldir%/foo_static_method_constant_property.m \
